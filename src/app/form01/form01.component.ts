e29e74a963195ee242a0e6417c6a8533e82ebce8
unknown <oguzhansahin95@outlook.com>
2018-04-28 01:10:17 +0300

    first commit

---
+++ src/app/form01/form01.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  // tslint:disable-next-line:component-selector
+  selector: 'form01',
+  templateUrl: './form01.component.html',
+  styleUrls: ['./form01.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class Form01Component implements OnInit {
+
+  model = {
+    firstname: '',
+    lastname: ''
+   };
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+}
